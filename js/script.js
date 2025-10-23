79177b4431e48d99c0433c17eefecff24efb60b1
victoriajuarez244 <104036975+victoriajuarez244@users.noreply.github.com>
2025-10-23 18:30:13 -0300

    preloader simple

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Preloader simple 
+window.addEventListener('load', function() {
+    const preloader = document.getElementById('preloader');
+    if (preloader) {
+        preloader.style.opacity = '0';
+        setTimeout(() => {
+            preloader.style.display = 'none';
+        }, 500);
+    }
+});
